29becb4c250f4af74166ed52e63e1cba7e7bad83
Kevin Gosse <krix33@gmail.com>
2024-12-22 21:12:56 +0100

    Add project

---
+++ src/Sample/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Sample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var calculator = new Calculator();
+        using var nativeCalculator = NativeObjects.ICalculator.Wrap(calculator);
+        CallNativeCalculator(nativeCalculator.Object);
+    }
+
+    static void CallNativeCalculator(IntPtr ptr)
+    {
+        var calculator = new NativeObjects.ICalculatorInvoker(ptr);
+        var result = calculator.Add(2, 3);
+
+        Console.WriteLine($"2 + 3 = {result}");
+    }
+}
+
+[NativeObject]
+public interface ICalculator
+{
+    int Add(int value1, int value2);
+}
+
+public class Calculator : ICalculator
+{
+    public int Add(int value1, int value2) => value1 + value2;
+}
